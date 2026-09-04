af81ef7227f604bc0173f00fb38a7e77f93f5fac
Chris Feragotti <chrisferagotti@gmail.com>
2026-09-04 08:56:03 -0400

    CF: drop internal _build toolchain from the published recipe package

--- _build/scripts/save-scripts.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-D=/home/chris/projects/com.chrisferagotti.unsettlingtrend/recipes/trend_personal/_build/scripts
-mkdir -p "$D"
-cp /home/chris/projects/trend-*.sh /home/chris/projects/check-template.sh /home/chris/projects/save-scripts.sh "$D"/ 2>/dev/null
-ls "$D"
